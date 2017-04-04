bae40fcc8b71e1d7707fc4ad364dd261f95e7e14
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-04-04 10:15:42 -0400

    hangman 2 modified

--- src/Hangman2.java
+++ src/Hangman2.java
@@ -24,6 +24,9 @@ public class Hangman2 {
         //store Player 1 input that will be later converted into '-'
         String word = input.nextLine();
 
+        //convert word to lowercase
+        word = word.toLowerCase();
+
         //store word in letters
         String secretWord = word;
 
@@ -40,12 +43,55 @@ public class Hangman2 {
             String temp = word.substring(i, i + 1);
             word = word.replace(temp, "-");
         }
-        
-        //print out word with replaced dashes
-        System.out.println("word");
-        
-        //create variables 
-        
 
+        //print out word with replaced dashes
+        System.out.println(word);
+
+        //create variables 
+        //create counter to keep track of position
+        int counter = 1;
+        //create counter to keep track the number of lives
+        int lives = 6;
+        //check if user guessed correctly or not
+        boolean correct = true;
+        //check to see if game is still running or not
+        boolean finished = false;
+
+        //create a StringBuilder to manipulate strings easier
+        StringBuilder build = new StringBuilder(word);
+
+        //create a loop to keep game running
+        while (lives > 0 && !finished) {
+            //Initiate player 2 to guess
+            System.out.println("Player 2: You have " + lives + " lives left. Guess a letter.");
+
+            //take Player 2's guess and store as string
+            String guess = input.nextLine();
+
+            //convert letter entered into lowercase
+            guess = guess.toLowerCase();
+
+            //check first letter entered if there are multiple letters entered
+            char guessCharacter = guess.charAt(0);
+
+            //create a loop to check Player 2's guess matches up with Player 1's mystery word
+            for (int i = 0; i < build.length(); i++) {
+
+                //guess is correct, replace guessed letter in position of the word
+                if (guessCharacter == secretWord.charAt(i)) {
+                    build.setCharAt(i, guessCharacter);
+                    correct = false;
+                }
+            }
+            //reveal guessed letter in the word
+            word = build.toString();
+            System.out.println(word);
+        }
+        if (correct == true) {
+            System.out.println("Congrats! You got a correct letter!");
+        } else {
+            System.out.println("You are incorrect. Try again!");
+            lives--;
+        }
     }
 }
